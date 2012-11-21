3bae69893a14f52985f2c04e36e2bce48ee24e32
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-11-21 10:26:09 -0500

    reorganized files into proper places

--- go/src/octopi/api/messageapi/messageapi.go
+++ go/src/octopi/api/messageapi/messageapi.go
@@ -1,84 +1,83 @@
 package messageapi
 
-import(
+import (
 	"container/list"
 )
 
 /* constants for contacting register server */
-const(
+const (
 	BROKER = iota
 	PRODUCER
 	CONSUMER
 )
 
 /* constants for register to tell which role a broker is taking */
-const(
+const (
 	LEADER = iota
 	FOLLOWER
 )
 
 /* used by brokers to contact register */
-type BrokerRegInit struct{
+type BrokerRegInit struct {
 	MessageSrc int
-	HostPort string
+	HostPort   string
 }
 
 /* used by register to assign brokers */
-type RegBrokerInit struct{
-	Role int
-	LeadUrl string //url of the leader, for followers
-	LeadOrigin string //origin of the leader, for followers
-	Brokers *list.List //list of associated brokers used if new leader
-	Topics *list.List //topics assigned by the register server 
+type RegBrokerInit struct {
+	Role       int
+	LeadUrl    string     //url of the leader, for followers
+	LeadOrigin string     //origin of the leader, for followers
+	Brokers    *list.List //list of associated brokers used if new leader
+	Topics     *list.List //topics assigned by the register server
 }
 
 /* used by producers to contact register */
-type ProdRegInit struct{
+type ProdRegInit struct {
 	MessageSrc int
-	Topic string
+	Topic      string
 }
 
 /* used by register to tell producer the lead broker */
-type RegProdInit struct{
-	LeadUrl string
+type RegProdInit struct {
+	LeadUrl    string
 	LeadOrigin string
 }
 
 /* used by consumers to contact register */
-type ConsRegInit struct{
+type ConsRegInit struct {
 	MessageSrc int
-	Topic string
+	Topic      string
 }
 
 /* used by register to tell consumer its assigned broker */
-type RegConsInit struct{
-	BrokerUrl string
+type RegConsInit struct {
+	BrokerUrl    string
 	BrokerOrigin string
 }
 
 /* used by followers to contact leader */
-type FollowLeadInit struct{
+type FollowLeadInit struct {
 	MessageSrc int
-	HostPort string
+	HostPort   string
 	//TODO: other fields to identify position of log
 }
 
-type ProdLeadInit struct{
+type ProdLeadInit struct {
 	MessageSrc int
-	HostPort string
-	Topic string
+	HostPort   string
+	Topic      string
 }
 
-type ConsBrokerInit struct{
+type ConsBrokerInit struct {
 	MessageSrc int
-	HostPort string
-	Topic string
+	HostPort   string
+	Topic      string
 	//TODO: other fields to identify position of wanted
 }
 
 /* used by producers to send publications to brokers*/
-type PubMsg struct{
-	Topic string
+type PubMsg struct {
+	Topic   string
 	Message string
 }
-
--- go/src/octopi/run/broker/broker.go
+++ go/src/octopi/run/broker/broker.go
@@ -1,49 +1,49 @@
 package main
 
-import(
+import (
 	"code.google.com/p/go.net/websocket"
-	"octopi/impl/brokerimpl"
-	"octopi/api/messageapi"
-	"net/http"
 	"fmt"
+	"net/http"
+	"octopi/api/messageapi"
+	"octopi/impl/brokerimpl"
 	"os"
 )
 
 var broker *brokerimpl.Broker
 
-func registerBroker(regUrl string, regOrigin string, myHostPort string) *brokerimpl.Broker{
+func registerBroker(regUrl string, regOrigin string, myHostPort string) *brokerimpl.Broker {
 	/* connect to register server */
 	regconn, err := websocket.Dial(regUrl, "", regOrigin)
 	/* fatal error if connection or messages failed */
 	checkError(err)
-	
+
 	bri := messageapi.BrokerRegInit{messageapi.BROKER, myHostPort}
 	/* send relevant broker information to register */
 	err = websocket.JSON.Send(regconn, bri)
 	checkError(err)
-	
+
 	/* receive register assignments */
 	var rbi messageapi.RegBrokerInit
 	err = websocket.JSON.Receive(regconn, &rbi)
 	checkError(err)
-	
+
 	/* create the broker based on JSON from register server */
 	b, err := brokerimpl.NewBroker(rbi, regconn)
 	checkError(err)
-	
+
 	/* close the connection if broker not assigned as leader */
-	if b.Role() != messageapi.LEADER{
+	if b.Role() != messageapi.LEADER {
 		regconn.Close()
 	}
 
 	return b
 }
 
-func ProdHandler(ws *websocket.Conn){
+func ProdHandler(ws *websocket.Conn) {
 	var pli messageapi.ProdLeadInit
 	err := websocket.JSON.Receive(ws, &pli)
 	/* close the connection and return if invalid request */
-	if nil != err || pli.MessageSrc!=messageapi.PRODUCER{
+	if nil != err || pli.MessageSrc != messageapi.PRODUCER {
 		ws.Close()
 		return
 	}
@@ -52,7 +52,7 @@ func ProdHandler(ws *websocket.Conn){
 	for {
 		var pubMsg messageapi.PubMsg
 		err := websocket.JSON.Receive(ws, &pubMsg)
-		if nil != err{
+		if nil != err {
 			broker.RemoveProd(pli)
 			ws.Close()
 			return
@@ -62,39 +62,39 @@ func ProdHandler(ws *websocket.Conn){
 	}
 }
 
-func ConsHandler(ws *websocket.Conn){
+func ConsHandler(ws *websocket.Conn) {
 	var cbi messageapi.ConsBrokerInit
 	err := websocket.JSON.Receive(ws, &cbi)
-	if nil != err||cbi.MessageSrc!=messageapi.CONSUMER{
+	if nil != err || cbi.MessageSrc != messageapi.CONSUMER {
 		ws.Close()
 		return
 	}
 	//TODO: send catchup if not
 	/* failed because topic does not exist */
 	err = broker.RegCons(ws, cbi)
-	if err!=nil{
+	if err != nil {
 		ws.Close()
 		return
 	}
-	
+
 	//TODO: preserve connection through blocking call. use receive?
 }
 
-func BrokerHandler(ws *websocket.Conn){
+func BrokerHandler(ws *websocket.Conn) {
 	var fli messageapi.FollowLeadInit
 	err := websocket.JSON.Receive(ws, &fli)
-	if nil !=err||fli.MessageSrc!=messageapi.BROKER{
+	if nil != err || fli.MessageSrc != messageapi.BROKER {
 		ws.Close()
 		return
 	}
-	
+
 }
 
-func main(){
+func main() {
 	//TODO: add command line arguments for register
 	broker := registerBroker("", "", "")
-	
-	if broker.Role()==messageapi.LEADER{
+
+	if broker.Role() == messageapi.LEADER {
 		http.Handle("/prodconn", websocket.Handler(ProdHandler))
 		http.Handle("/brokerconn", websocket.Handler(BrokerHandler))
 	}
@@ -102,8 +102,8 @@ func main(){
 	http.ListenAndServe(":12345", nil)
 }
 
-func checkError(err error){
-	if err!=nil{
+func checkError(err error) {
+	if err != nil {
 		fmt.Println("Fatal Error ", err.Error())
 		os.Exit(1)
 	}
