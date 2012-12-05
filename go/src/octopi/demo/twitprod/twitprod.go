b2c925e88964f41871ddb09cde629a608a900003
Andrew Chung <afchung@andrew.cmu.edu>
2012-12-05 11:51:04 -0500

    switched to demo package

---
+++ go/src/octopi/demo/twitprod/twitprod.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"octopi/demo/twitproducer"
+	"octopi/util/log"
+)
+
+func main() {
+	var broker = flag.String("register", "localhost:12345", "host and port number of broker")
+	flag.Parse()
+
+	tp, err := twitproducer.NewTwitProducer("octopx", "octopioctopus", *broker, nil)
+
+	if nil != err {
+		log.Warn("Did not receive a correct twitproducer")
+	}
+
+	for {
+		_, err = tp.RelayMessages(10)
+
+		if nil != err {
+			log.Warn("Did not send all 10 messages, error = %v", err)
+			break
+		}
+	}
+}
